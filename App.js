4a30231941aa74cb3052e6dc7ba1f7f6ad1a6831
Marina Tanaka <mtanakasg@gmail.com>
2024-03-25 08:43:11 -0300

    Estruturação basica do componente

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar, StyleSheet, View } from "react-native";
+
+// Importação da biblioteca de mapa
+import MapView from "react-native-maps";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Location com MapView</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar />
+      <View style={estilos.container}>
+        <MapView style={estilos.map} />
+      </View>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
+const estilos = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  },
+
+  map: {
+    width: "100%",
+    height: "100%",
   },
 });
