d0b7faf1ccf3355e7149829449c1f63d83b9ed8e
Marina Tanaka <mtanakasg@gmail.com>
2024-03-25 09:18:56 -0300

    Adicionado coordenada fixa para maker

--- App.js
+++ App.js
@@ -1,17 +1,30 @@
 import { StatusBar, StyleSheet, View } from "react-native";
 
-// Importação da biblioteca de mapa
-import MapView from "react-native-maps";
+// Importação da biblioteca de mapa e o sub-componente "Marker"
+import MapView, { Marker } from "react-native-maps";
 
 export default function App() {
-  // Cordenadas fixas
+  // Coordenadas fixas para o componente "mapview"
   const regiaoInicial = {
-    latitude: -10,
-    longitude: -55,
+    // Brasil
+    // latitude: -10,
+    // longitude: -55,
+
+    // São Paulo
+    latitude: -23.5489,
+    longitude: -46.6388,
 
     // Zoom no Mapa, quanto menor o numero mais proxima da localização escolhida e quanto maior o numero mais longe da localização escolhida.
-    latitudeDelta: 1,
-    longitudeDelta: 1,
+    latitudeDelta: 0.8,
+    longitudeDelta: 0.8,
+  };
+
+  // Coordenadas fixas para o sub-componente "marker"
+  const localizacao = {
+    latitude: -33.867886,
+    longitude: -63.987,
+    latitudeDelta: 0.8,
+    longitudeDelta: 0.8,
   };
 
   return (
@@ -22,9 +35,16 @@ export default function App() {
           style={estilos.mapa}
           initialRegion={regiaoInicial} // Regiao Inicial
           mapType="hybrid" // Tipo do mapa
-          maxZoomLevel={30} // Zoom maximo para usuario
-          minZoomLevel={5} // Zoom minimo para usuario
-        />
+          // maxZoomLevel={30} // Zoom maximo para usuario
+          // minZoomLevel={5} // Zoom minimo para usuario
+        >
+          <Marker
+            coordinate={localizacao} // Coordenada
+            title="Você está aqui!" // Titulo ao clicar no marker
+            pinColor="blue" // Cor do pin
+            draggable // Arrastavel
+          />
+        </MapView>
       </View>
     </>
   );
